428d6eae66e6eacb4a51a5088e5aa282bd070064
Zackory Kelly <36438044+zackorykelly@users.noreply.github.com>
2024-11-29 01:21:03 -0500

    formatting

--- src/utils.test.ts
+++ src/utils.test.ts
@@ -1,6 +1,6 @@
-import { add } from "./utils";
-import { test, expect } from "vitest";
+import { add } from './utils';
+import { test, expect } from 'vitest';
 
-test("add", () => {
+test('add', () => {
   expect(add(1, 2)).toBe(3);
 });
--- tsup.config.ts
+++ tsup.config.ts
@@ -2,11 +2,11 @@ import { defineConfig } from 'tsup';
 //random comment
 
 export default defineConfig({
-    entry: ['src/index.ts'],
-    format: ['esm', 'cjs'],
-    dts: true,
-    outDir: 'dist',
-    sourcemap: true,
-    splitting: false,
-    clean: true,
-})
+  entry: ['src/index.ts'],
+  format: ['esm', 'cjs'],
+  dts: true,
+  outDir: 'dist',
+  sourcemap: true,
+  splitting: false,
+  clean: true,
+});
